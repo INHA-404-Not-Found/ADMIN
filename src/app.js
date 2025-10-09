5fc0c5964a64dcddc3c82bbb05d6b430be166840
eheka78 <gemddkim22@gmail.com>
2025-10-10 08:45:17 +0900

    feat: 라우터 적용

--- src/app.js
+++ src/app.js
@@ -1,13 +1,25 @@
-//import Login from "./components/login/Login";
-import Main from "./components/admin/Main";
+import LoginPage from "./pages/LoginPage";
+import MainPage from "./admin/Main";
+import PostDetailFrame from "./components/PostDetailFrame";
+import PostDetailFrameEdit from "./components/PostDetailEditFrame";
+import AdminManagementPage from "./pages/AdminManagementPage";
+
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div
-      className="App"
-      style={{ height: "100%" }}
-    >
-      <Main />
+    <div className="App" style={{ height: "100%" }}>
+      <Router>
+        <Routes>
+          <Route path="/login" element={<LoginPage type="login" />} />
+
+          <Route path="/" element={<MainPage />} />
+          <Route path="/adminManagement" element={<AdminManagementPage />} />
+          <Route path="/postDetail" element={<PostDetailFrame />} />
+          <Route path="/postDetailFrameEdit" element={<PostDetailFrameEdit />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
