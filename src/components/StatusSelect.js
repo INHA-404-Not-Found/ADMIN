e5867908a75373b706f6a8a2e239da28264f5797
eheka78 <gemddkim22@gmail.com>
2025-10-10 08:40:22 +0900

    feat: 상태(완료, 미완료, 인계됨) 컴포넌트 생성

---
+++ src/components/StatusSelect.js
@@ -0,0 +1,9 @@
+export default function StatusSelect () {
+    return (
+        <select id="stateType">
+            <option id="stateType">미완료</option>
+            <option id="stateType">완료</option>
+            <option id="stateType">인계됨</option>
+        </select>
+    );
+};
