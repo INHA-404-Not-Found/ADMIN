9ca785b9b8306bdfd8c685cd2fdff2944fb01dba
eheka78 <gemddkim22@gmail.com>
2025-10-10 20:08:03 +0900

    feat: 폰트 뷰 생성

---
+++ src/fonts/FontTest.js
@@ -0,0 +1,34 @@
+export default function FontTest() {
+    return (
+        <div style={{ fontSize: "30px" }}>
+            <div style={{ fontFamily: 'Pretendard-Black' }}>
+                123456789 폰트 테스트 : Pretendard-Black ./*$&^#@+_-()[]!?~`'";:
+            </div>
+            <div style={{ fontFamily: 'Pretendard-ExtraBold' }}>
+                123456789 폰트 테스트 : Pretendard-ExtraBold ./*$&^#@+_-()[]!?~`'";:
+            </div>
+            <div style={{ fontFamily: 'Pretendard-Bold' }}>
+                123456789 폰트 테스트 : Pretendard-Bold ./*$&^#@+_-()[]!?~`'";:
+            </div>
+            <div style={{ fontFamily: 'Pretendard-SemiBold' }}>
+                123456789 폰트 테스트 : Pretendard-SemiBold ./*$&^#@+_-()[]!?~`'";:
+            </div>
+            <div style={{ fontFamily: 'Pretendard-Medium' }}>
+                123456789 폰트 테스트 : Pretendard-Medium ./*$&^#@+_-()[]!?~`'";:
+            </div>
+            <div style={{ fontFamily: 'Pretendard-Regular' }}>
+                123456789 폰트 테스트 : Pretendard-Regular ./*$&^#@+_-()[]!?~`'";:
+            </div>
+            <div style={{ fontFamily: 'Pretendard-Light' }}>
+                123456789 폰트 테스트 : Pretendard-Light ./*$&^#@+_-()[]!?~`'";:
+            </div>
+            <div style={{ fontFamily: 'Pretendard-ExtraLight' }}>
+                123456789 폰트 테스트 : Pretendard-ExtraLight ./*$&^#@+_-()[]!?~`'";:
+            </div>
+            <div style={{ fontFamily: 'Pretendard-Thin' }}>
+                123456789 폰트 테스트 : Pretendard-Thin ./*$&^#@+_-()[]!?~`'";:
+            </div>
+        </div>
+    )
+
+}
