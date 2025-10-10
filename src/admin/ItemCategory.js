65196eac706e4eb0570eca14275b5f28365c33dd
eheka78 <gemddkim22@gmail.com>
2025-10-11 03:35:43 +0900

    feat: 물품 카테고리 관리 페이지 생성

---
+++ src/admin/ItemCategory.js
@@ -0,0 +1,60 @@
+import StatusSelect from "../components/StatusSelect";
+import styles from "../styles/Mainboard.module.css";
+
+import { Link } from "react-router-dom";
+
+export default function ItemCategory() {
+    {/*메인보드*/}
+    return (
+        <div>
+            <div style={{ fontFamily:"pretendard-SemiBold", fontSize:"25px" }}>물품 카테고리 관리</div>
+
+            {/*검색창*/}
+            <div>
+                <input placeholder="물품 카테고리 추가"></input>
+                <button type="submit">추가하기</button>
+            </div>
+
+            <br />
+
+            {/*물품 카테고리 리스트*/}
+            <div>
+                {/*테이블*/}
+                <div>
+                    <table>
+                        <thead>
+                            <tr>
+                                {/* {columns.map((column) => (
+                                    <th key={column}>{column}</th>
+                                ))} */}
+                                <th>번호</th>
+                                <th>카테고리</th>
+                                <th>수정하기</th>
+                                <th>삭제하기</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {/* {data.map(({ name, email, phone }) => (
+                                <tr key={name + email + phone}>
+                                    <td>{name}</td>
+                                    <td>{email}</td>
+                                    <td>{phone}</td>
+                                </tr>
+                            ))} */}
+                            {Array.from({ length: 10 }).map((_, i) => (
+                                <tr key={i}>
+                                    <td>{i+1}</td>
+                                    <td>지갑(현금, 카드)</td>
+                                    <td>수정하기</td>
+                                    <td>삭제하기</td>
+                                </tr>
+                                ))}
+                            
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    )
+
+}
---
+++ src/pages/ItemCategoryPage.js
@@ -0,0 +1,36 @@
+import ItemCategory from "../admin/ItemCategory";
+import Main from "../admin/Main";
+import Logo from "../components/Logo";
+import MenuPage from "../components/MenuPage";
+import styles from "../styles/Main.module.css";
+
+import { Link } from "react-router-dom";
+
+export default function ItemCategoryPage() {
+    return (
+        <div
+            style={{
+                display: "grid",
+                gridTemplateColumns: "1fr 5fr",
+                gap: "20px",
+
+                height: "100%"
+            }}
+        >
+            {/*왼쪽 메뉴*/}
+            <div style={{margin:"0 10%"}}>
+                {/*로고*/}
+                <Logo />
+
+                <br></br>
+
+                {/*메뉴*/}
+                <MenuPage />
+            </div>
+            
+            {/*메인보드*/}
+            <ItemCategory />
+        </div>
+    )
+
+}
