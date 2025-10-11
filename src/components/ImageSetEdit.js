0640747ce2f676f587408914d5469a98525def39
eheka78 <gemddkim22@gmail.com>
2025-10-12 08:11:53 +0900

    feat: ImageSet 컴포넌트 생성

---
+++ src/components/ImageSetEdit.js
@@ -0,0 +1,19 @@
+export default function ImageSetEdit(){
+
+    {/*이미지 셋 화면*/}
+    return (
+        <div>
+            <div> 
+                <img src="./images/left.png" alt="left"/>
+                
+                <img src="./images/" alt="img"/>
+                
+                <img src="./images/right.png" alt="right"/>
+            </div>
+            
+            <div>
+                <button>사진 업로드</button>
+            </div>
+        </div>
+    )
+}
