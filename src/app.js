5adad58f446ddc68218ad859f41f6794264307dd
eheka78 <gemddkim22@gmail.com>
2025-10-12 08:24:13 +0900

    refactor: 라우터 변경

--- src/app.js
+++ src/app.js
@@ -1,14 +1,10 @@
 import LoginPage from "./pages/LoginPage";
 import MainPage from "./pages/MainPage";
-import PostDetailFrame from "./components/PostDetailFrame";
-import PostDetailFrameEdit from "./components/PostDetailEditFrame";
-import AdminManagementPage from "./pages/AdminManagementPage";
-import PostRegistPage from "./pages/PostRegistPage";
+import ItemCategoryPage from "./pages/ItemCategoryPage";
+import FontTest from "./fonts/FontTest";
 
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import FontTest from "./fonts/FontTest";
-import ItemCategoryPage from "./pages/ItemCategoryPage";
 
 function App() {
   return (
@@ -18,11 +14,7 @@ function App() {
           <Route path="/login" element={<LoginPage type="login" />} />
 
           <Route path="/" element={<MainPage />} />
-          {/* <Route path="/adminManagement" element={<AdminManagementPage />} /> */}
           <Route path="/itemCategory" element={<ItemCategoryPage />} />
-          <Route path="/postDetailFrame" element={<PostDetailFrame />} />
-          <Route path="/postDetailFrameEdit" element={<PostDetailFrameEdit />} />
-          <Route path="/postRegist" element={<PostRegistPage />} />
           <Route path="/fontTest" element={<FontTest />} />
         </Routes>
       </Router>
