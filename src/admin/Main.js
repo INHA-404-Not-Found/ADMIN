e05994d93460404c6533f5bd592c7d8302e7d9fb
eheka78 <gemddkim22@gmail.com>
2025-10-12 08:20:53 +0900

    chore: 여러 사항 변경

--- src/admin/Main.js
+++ src/admin/Main.js
@@ -1,13 +1,21 @@
 import StatusSelect from "../components/StatusSelect";
+import PopUpFrame from "../components/PopUpFrame";
 import styles from "../styles/Mainboard.module.css";
-import "../styles/Table.css";
+import tableStyles from "../styles/Table.module.css";
 
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function Main() {
+    const [showPopUp, setShowPopUp] = useState(false);
+    const [type, setType] = useState("");
+
+    useEffect (() => {
+        console.log(type);
+    }, [type])
+
     {/*메인보드*/}
     return (
-                
         <div className={styles.Container}>
             <div className={styles.Title_Container}>Dashboard</div>
 
@@ -63,7 +71,7 @@ export default function Main() {
 
                 {/*테이블*/}
                 <div>
-                    <table className={styles.Table}>
+                    <table className={tableStyles.Table}>
                         <thead>
                             <tr>
                                 {/* {columns.map((column) => (
@@ -72,6 +80,7 @@ export default function Main() {
                                 <th><input type="checkbox" /></th>
                                 <th>번호</th>
                                 <th>날짜</th>
+                                <th>습득/분실</th>
                                 <th>제목</th>
                                 <th>카테고리</th>
                                 <th>작성자</th>
@@ -88,25 +97,48 @@ export default function Main() {
                                 </tr>
                             ))} */}
                             {Array.from({ length: 10 }).map((_, i) => (
-                                <tr key={i}>
-                                    <td><input type="checkbox" /></td>
-                                    <td>{i+1}</td>
-                                    <td>2025.10.10.금</td>
-                                    <td style={{ textAlign: "left" }}>
-                                        <Link to="/postDetailFrame">검정색 지갑 하텍 강당에서 분실하였습니다.</Link>
-                                    </td>
-                                    <td>지갑</td>
-                                    <td>김도담</td>
-                                    <td>
-                                        <StatusSelect />
-                                    </td>
-                                </tr>
+                                <>
+                                    <tr key={i}>
+                                        <td><input type="checkbox" /></td>
+                                        <td>{i+1}</td>
+                                        <td>2025.10.10.금</td>
+                                        <td>분실</td>
+                                        <td style={{ textAlign: "left", padding: "0 5px" }}
+                                            onClick={() => {setShowPopUp(true); setType("lost post");}}
+                                        >
+                                            검정색 지갑 하텍 강당에서 분실하였습니다.
+                                        </td>
+                                        <td>지갑</td>
+                                        <td>김도담</td>
+                                        <td>
+                                            <StatusSelect />
+                                        </td>
+                                    </tr>
+                                    <tr key={i+1}>
+                                        <td><input type="checkbox" /></td>
+                                        <td>{i+2}</td>
+                                        <td>2025.10.10.금</td>
+                                        <td>습득</td>
+                                        <td style={{ textAlign: "left", padding: "0 5px" }} 
+                                            onClick={() => {setShowPopUp(true); setType("gain post");}}
+                                        >
+                                            검정색 지갑 찾음.
+                                        </td>
+                                        <td>지갑</td>
+                                        <td>김도담</td>
+                                        <td>
+                                            <StatusSelect />
+                                        </td>
+                                    </tr>
+                                </>
                                 ))}
                             
                         </tbody>
                     </table>
                 </div>
             </div>
+
+            {showPopUp && <PopUpFrame type={type} setType={setType} onClose={() => setShowPopUp(false)} />}
         </div>
     )
 
