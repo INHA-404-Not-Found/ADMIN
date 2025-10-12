5af71864c7040b633d563f4d39f431db749dfac5
eheka78 <gemddkim22@gmail.com>
2025-10-12 10:19:01 +0900

    data 적용

--- src/post/GainTableEdit.js
+++ src/post/GainTableEdit.js
@@ -1,11 +1,13 @@
+import { ITEM } from "../assets/ItemAsset";
 import tableStyles from "../styles/Table2.module.css";
+import checkboxStyle from "../styles/CheckboxLabel.module.css";
 
 
 export default function GainTableEdit(){
 
     return (
         <>
-            <table className={tableStyles.Table}>
+            <table className={tableStyles.Table2}>
                 <tbody>          
                     <tr>
                         <th>습득물에 학번 존재 유무</th>
@@ -17,41 +19,46 @@ export default function GainTableEdit(){
                     <tr>
                         <th>물품 카테고리</th>
                         <td>
-                            {["지갑", "열쇠", "카드", "기타"].map((item, index) => (
-                                <label key={index}>
-                                    <input type="checkbox" />
-                                    {item}
-                                </label>
-                            ))}
+                            <div className={checkboxStyle.Checkbox_Style}>
+                                {ITEM.map((e) => (
+                                    <label key={e}><input type="checkbox" /><span>{e}</span></label>
+                                ))}
+                            </div>
                         </td>
                     </tr>
                     <tr>
                         <th>습득 장소</th>
                         <td>
-                            <input />
+                            <select id="location">
+                                <option id="location">후문</option>
+                                <option id="location">정문</option>
+                                <option id="location">5호관</option>
+                                <option id="location">60주년기념관</option>
+                                <option id="location">하이테크관</option>
+                            </select>
                         </td>
                     </tr>
                     <tr>
                         <th>보관 위치</th>
                         <td>
-                            <input />
+                            <input type="text" />
                         </td>
                     </tr>
                 </tbody>
             </table>
             
-            <table className={tableStyles.Table}>
+            <table className={tableStyles.Table2}>
                 <tbody>
                     <tr>
                         <th>제목</th>
                         <td>
-                            <input />
+                            <input type="text" />
                         </td>
                     </tr>
                     <tr>
                         <th>내용</th>
                         <td>
-                            <input defaultValue="제곧내" />
+                            <textarea></textarea>
                         </td>
                     </tr>
                 </tbody>
--- src/post/LostTableEdit.js
+++ src/post/LostTableEdit.js
@@ -1,43 +1,50 @@
+import { ITEM } from "../assets/ItemAsset";
 import tableStyles from "../styles/Table2.module.css";
+import checkboxStyle from "../styles/CheckboxLabel.module.css";
 
 export default function LostTableEdit(){
 
     return (
         <>
-            <table className={tableStyles.Table}>
+            <table className={tableStyles.Table2}>
                 <tbody>                
                     <tr>
                         <th>물품 카테고리</th>
                         <td>
-                            {["지갑", "열쇠", "카드", "기타"].map((item, index) => (
-                                <label key={index}>
-                                    <input type="checkbox" />
-                                    {item}
-                                </label>
-                            ))}
+                            <div className={checkboxStyle.Checkbox_Style}>
+                                {ITEM.map((e) => (
+                                    <label key={e}><input type="checkbox" /><span>{e}</span></label>
+                                ))}
+                            </div>
                         </td>
                     </tr>
                     <tr>
                         <th>분실 장소</th>
                         <td>
-                            <input />
+                            <select id="location">
+                                <option id="location">후문</option>
+                                <option id="location">정문</option>
+                                <option id="location">5호관</option>
+                                <option id="location">60주년기념관</option>
+                                <option id="location">하이테크관</option>
+                            </select>
                         </td>
                     </tr>
                 </tbody>
             </table>
 
-            <table className={tableStyles.Table}>
+            <table className={tableStyles.Table2}>
                 <tbody>
                     <tr>
                         <th>제목</th>
                         <td>
-                            <input />
+                            <input type="text" />
                         </td>
                     </tr>
                     <tr>
                         <th>내용</th>
                         <td>
-                            <input />
+                            <textarea></textarea>
                         </td>
                     </tr>
                 </tbody>
