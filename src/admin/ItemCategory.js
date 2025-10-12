4c43a2460de9aa1d87e2d590320b28e36bb845bc
eheka78 <gemddkim22@gmail.com>
2025-10-13 04:35:39 +0900

    feat: alert 추가

--- src/admin/ItemCategory.js
+++ src/admin/ItemCategory.js
@@ -7,11 +7,6 @@ import { Link } from "react-router-dom";
 
 export default function ItemCategory() {
 
-    function categoryEdit(i){
-        const categoryName = prompt("수정할 카테고리 이름을 적으세요");
-        
-    }
-
     {/*메인보드*/}
     return (
         <div className={styles.Container}>
@@ -22,7 +17,14 @@ export default function ItemCategory() {
             {/*검색창*/}
             <div className={styles.Add_Input}>
                 <input placeholder="물품 카테고리 추가"></input>
-                <button type="submit">추가하기</button>
+                <button
+                    type="submit"
+                    onClick={() => {
+                        alert("카테고리가 추가되었습니다.");
+                    }}
+                >
+                    추가하기
+                </button>
             </div>
 
             <br></br>
@@ -56,11 +58,30 @@ export default function ItemCategory() {
                                     <td style={{ textAlign:"center" }}>{i+1}</td>
                                     <td style={{ textAlign:"center" }}>지갑(현금, 카드)</td>
                                     <td style={{ textAlign:"center" }}
-                                        onClick={() => categoryEdit(i)}
+                                        onClick={() => {
+                                            const categoryName = prompt("수정할 카테고리 이름을 적으세요");
+                                            if(categoryName === ""){
+                                                alert("실패하였습니다.");
+                                            } else{
+                                                alert("추가되었습니다.")
+                                            }
+                                        }}
                                     >  
                                         수정하기
                                     </td>
-                                    <td style={{ textAlign:"center" }}>삭제하기</td>
+                                    <td
+                                        style={{ textAlign:"center" }}
+                                        onClick={() => {
+                                            const deleteConfirmMsg = prompt("삭제하면 복구할 수 없습니다. 삭제하려면 카테고리의 이름을 그대로 따라 적으세요.")
+                                            if(deleteConfirmMsg === "지갑(현금, 카드)"){
+                                                alert("삭제되었습니다.");
+                                            } else{
+                                                alert("실패하였습니다.")
+                                            }
+                                        }}   
+                                    >
+                                        삭제하기
+                                    </td>
                                 </tr>
                                 ))}
                             
