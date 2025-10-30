46b12974dae5194bb5b152cfa1229a79eaf92619
eheka78 <gemddkim22@gmail.com>
2025-10-30 22:39:11 +0900

    fix

--- src/api/receiver.js
+++ src/api/receiver.js
@@ -2,7 +2,7 @@ import api from "./api.js";
 
 
 // 수령인 등록
-export const registerReceiver = async () => {
+/*export const registerReceiver = async () => {
     try {
         const res = await api.post('/admin/receivers', {
             postId,
@@ -12,18 +12,16 @@ export const registerReceiver = async () => {
             studentId
         });
         console.log("registerReceiver: ", res.data.message, "[receiverId: ", res.data.receiverId, "]");
-        
-        navigate('/');
+    
     } catch (err) {
         console.error('에러 발생: ', err);
         alert("registerReceiver 실패");
     }
-
-    return response.data;
 };
-
+*/
 
 // 수령인 수정
+/*
 export const updateReceiver = async (receiver_id) => {
     try {
         const res = await api.patch('/admin/receivers/' + receiver_id, {
@@ -34,15 +32,12 @@ export const updateReceiver = async (receiver_id) => {
         });
         console.log("updateReceiver: ", res.data);
         
-        navigate('/');
     } catch (err) {
         console.error('에러 발생: ', err);
         alert("updateReceiver 실패");
     }
-
-    return response.data;
 };
-
+*/
 
 // 수령인 삭제
 export const deleteReceiver = async (receiver_id) => {
@@ -50,13 +45,10 @@ export const deleteReceiver = async (receiver_id) => {
         const res = await api.delete('/admin/receivers/' + receiver_id);
         console.log("deleteReceiver: ", res.data);
         
-        navigate('/');
     } catch (err) {
         console.error('에러 발생: ', err);
         alert("deleteReceiver 실패");
     }
-
-    return response.data;
 };
 
 
@@ -66,29 +58,27 @@ export const getReceiver = async (receiver_id) => {
         const res = await api.get('/admin/receivers/' + receiver_id);
         console.log("getReceiver: ", res.data);
         
-        navigate('/');
     } catch (err) {
         console.error('에러 발생: ', err);
         alert("getReceiver 실패");
     }
-
-    return response.data;
 };
 
 
 // 특정 게시물의 수령인 조회
-export const getReceiverByPost = async (post_id) => {
+export const getReceiverByPost = async (setReceiver, postId) => {
+    console.log("getReceiverByPost start");
+
     try {
-        const res = await api.get('/admin/receivers/posts/' + post_id);
+        const res = await api.get('/admin/receivers/posts/' + postId);
+        
+        setReceiver(res.data);
         console.log("getReceiverByPost: ", res.data);
         
-        navigate('/');
     } catch (err) {
         console.error('에러 발생: ', err);
         alert("getReceiverByPost 실패");
     }
-
-    return response.data;
 };
 
 
@@ -98,11 +88,8 @@ export const getAllReceivers = async (post_id) => {
         const res = await api.get('/admin/receivers/');
         console.log("getAllReceivers: ", res.data);
         
-        navigate('/');
     } catch (err) {
         console.error('에러 발생: ', err);
         alert("getAllReceivers 실패");
     }
-
-    return response.data;
 };
