88b798ad97afd2f1741bb01d87033594924297aa
eheka78 <gemddkim22@gmail.com>
2025-10-30 13:57:56 +0900

    feat: setupProxy 파일 생성

---
+++ src/setupProxy.js
@@ -0,0 +1,11 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function(app) {
+  app.use(
+    '/api',
+    createProxyMiddleware({
+      target: 'https://lost-inha.kro.kr/',
+      changeOrigin: true,
+    })
+  );
+};
